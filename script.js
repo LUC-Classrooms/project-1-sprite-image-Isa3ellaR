aa74fb66d0105e8fbf490fbece007dd34d7a962c
Isabella Rodriguez <157334109+Isa3ellaR@users.noreply.github.com>
2024-02-26 04:49:09 +0000

    I added a nose and made comments on my drawing

--- script.js
+++ script.js
@@ -8,31 +8,45 @@ function setup(){
 function draw(){
   background(255, 160, 255);
   // add your drawing code here ...
-  x++
+ //x++
   if(x>width){
     x=0
   }
   a += .01;
   push()
   translate(x, y)
-  rotate(a)
+  //rotate(a)
+
   //Ears
-  stroke(255, 160, 255);
-  fill(222, 44, 147)
-  arc(-75, -80, 100, 100, HALF_PI, TWO_PI);
-  arc(75, -80, 100, 100, PI, HALF_PI);
+  stroke(255, 160, 255); //light pink inside of ears
+  fill(222, 44, 147);  //magenta
+  arc(-75, -80, 100, 100, HALF_PI, TWO_PI);  //left ear
+  arc(75, -80, 100, 100, PI, HALF_PI);  //right ear
   stroke(252, 137, 185);
   fill(252, 137, 185);
-  ellipse(-75, -80, 75);
-  ellipse(75, -80, 75);
+  ellipse(-75, -80, 75);  //inside of left ear
+  ellipse(75, -80, 75);  //inside of right ear
+
   //Face
- fill(222, 44, 147);
+ fill(222, 44, 147); //magenta fill for face
  ellipse(0, 0, 175);
-  //eyes
+
+  //Eyes
   stroke(0);
-  fill(255)
-  ellipse(-50, -10, 50);
-  ellipse(50, -10, 50);
+  fill(255) //white inside of eyes
+  ellipse(-50, -10, 50);  //left eye
+  ellipse(50, -10, 50);  //right eye
+  fill(0)  //black for pupils 
+  ellipse(-50, -2, 35); //left pupil
+  ellipse(50, -2, 35);  //right pupil
+
+  //Nose
+  fill(255, 137, 240) //light pink
+  ellipse(0, 0, 20)
+
+  
+
+
   pop()
 
   
