b73847f2e33221e61a6b6e33c4ab6d43d86d359d
Isabella Rodriguez <157334109+Isa3ellaR@users.noreply.github.com>
2024-02-26 08:18:55 +0000

    I added a cheese wheel for my mouse to chase

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-var x, y, a = 0;
+var x, y, a, b = 0;
+var c, c1 = 0 //variables for cheese wheel 
 function setup(){
   createCanvas(600, 400);
   x=width/2
   y=height/2
+
+  c=width/4
+  c1=height/10
 }
 
 function draw(){
-  background(255, 160, 255);
+  background(255, 160, 255);  //pink background color
   // add your drawing code here ...
- //x++
+  //x++
   if(x>width){
     x=0
   }
@@ -43,6 +47,32 @@ function draw(){
   //Nose
   fill(255, 137, 240) //light pink
   ellipse(0, 0, 20)
+  
+  translate(c, c1) //new center point for cheese wheel
+  b+=.06
+  //rotate(b) //cheese wheel rolling away
+  
+  //Cheese wheel
+  fill(255, 202, 40)
+  stroke(255, 180, 0)
+  strokeWeight(2)
+  ellipse(0, 0, 80)
+  //Cheese holes
+  fill(255, 180, 0)
+  ellipse(-10, 0, 10)
+  ellipse(10, 0, 8)
+  ellipse(-12, 20, 8)
+  ellipse(12, 30, 10)
+  ellipse(-20, 30, 7)
+  ellipse(-20, -30, 10)
+  ellipse(-30, -10, 8)
+  ellipse(20, -10, 10)
+  ellipse(0, -20, 10)
+  ellipse(20, -30, 8)
+  ellipse(27, 20, 12)
+
+  
+
 
   
 
